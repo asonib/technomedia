8365d836c03b7b534fb875a12e21e04488c27ed0
Aryan Soni <aryan.soni@btech.christuniversity.in>
2020-08-01 20:56:57 +0530

    validation mods -encryption

--- routes/auth/register.js
+++ routes/auth/register.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs')
+
+require('../../model/Users')
+const Users = mongoose.model('user');
 
 router.post('/auth/register', [
     check('name', 'Name is required').isString(),
@@ -13,7 +18,15 @@ router.post('/auth/register', [
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
+    const user = await Users.findOne({email: req.email})
+    if(user){
+        return res.json({errors: [{'msg': 'Email Already Exist'}]})
+    }
 
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
+    return res.json({'msg': hash})
 
 
     console.log(req.body);
