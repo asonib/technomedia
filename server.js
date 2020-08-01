9adf5ed0fcbe4c7cda6feca824287b44ce855e01
Aryan Soni <aryan.soni@btech.christuniversity.in>
2020-08-01 20:21:07 +0530

    mongo integrated

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+
+mongoose.connect('mongodb://localhost/contact', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log('MongoDB Connected')
+}).catch((err) => {
+    console.error(`Error Connecting to DB ${err}`)
+})
 
 app.use('/api', require('./routes/auth/login'));
 app.use('/api', require('./routes/auth/register'));
