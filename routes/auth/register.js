5a6eb6710d2dc4eccc9c4a3836b754a4f8423c5a
Aryan Soni <aryan.soni@btech.christuniversity.in>
2020-08-01 20:41:04 +0530

    -express validator -register partial

--- routes/auth/register.js
+++ routes/auth/register.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator');
 
-router.get('/auth/register', (req, res) => {
-    res.send('Register')
+router.post('/auth/register', [
+    check('name', 'Name is required').isString(),
+    // username must be an email
+    check('email', 'Email is required').isEmail(),
+    // password must be at least 5 chars long
+    check('password', 'Password is required').isLength({ min: 6 })
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+
+
+    console.log(req.body);
+    return res.json({payload: req.body})
 })
 
 module.exports = router;
