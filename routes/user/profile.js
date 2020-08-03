70e9f6f7aa49c8fa392fcd8fd1eb1b4d0485550e
Aryan Soni <aryan.soni@btech.christuniversity.in>
2020-08-04 00:54:14 +0530

    profiles fetch added

--- routes/user/profile.js
+++ routes/user/profile.js
@@ -6,7 +6,28 @@ const { check, validationResult } = require('express-validator');
 
 require('../../models/Profile')
 const Profiles = mongoose.model('profile');
+require('../../models/Users')
+const Users = mongoose.model('user');
 
+/*
+    method: GET
+    visibility: private
+    response: profile of all registered user - JSON
+*/
+router.get('/user/all', auth, async(req, res) => {
+    try {
+        let profiles = await Profiles.find().populate('user', ['name', 'avatar', 'email', 'public_key'])
+        return res.json(profiles)
+    } catch (err) {
+        console.error('Server Error')
+        return res.json(res.status(400).json(err.message))
+    }    
+})
+/*
+    method: POST
+    visibility: private
+    response: profile of registered user - JSON
+*/
 router.post('/user/profile', [auth,
     check('department', 'Department Name is required').not().isEmpty(),
     // password must be at least 5 chars long
@@ -52,7 +73,7 @@ router.post('/user/profile', [auth,
     try {
         let profile = await Profiles.findOne({ user: req.user });
         if (profile) {
-            const profile = await Profiles.findOneAndUpdate({ user: req.user }, { $set: profileDetails }, { new: true });
+            const profile = await Profiles.findOneAndUpdate({ user: req.user }, { $set: profileDetails }, { new: true }).populate('user', ['name', 'avatar', 'email','public_key']);
             return res.status(200).json(profile);
         }
         profile = new Profiles(profileDetails);
